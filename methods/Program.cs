6536579c742f31949fa9408932fcda9b68562ed1
Sonalika <sonalika011@gmail.com>
2023-09-12 05:38:20 +0530

    Add project files.

---
+++ methods/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("I am an Argument and called from a method");
+            Console.Read();
+        }
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
